75b8b81a57187e5298a30ff2857be99c04ec75ad
Viku4780 <vikrantmom5748@gmail.com>
2025-05-21 04:27:12 +0000

    learn further dataTypes

--- 01_basics/03_dataTypeConversion.js
+++ 01_basics/03_dataTypeConversion.js
@@ -15,7 +15,7 @@ let valueInNumber = Number(score);
 let isLoggedIn = "hitesh"
 
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 //1 => ture; 0 => false
 //"" => false
@@ -24,5 +24,65 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);//joh pehle aayega uske hisaab se hoga process output- "122"
+console.log(1 + 2 + "2");//output-"32"
+
+console.log(true);//output-true
+console.log(+true);//output-1   The + symbol is the unary plus operator, which tries to convert the value to a number.
+
+/*
++true === 1
+
+JavaScript has type coercion — it automatically tries to convert values from one type to another when needed.
+
+Value	+value (numeric conversion)
+true	1
+false	0
+null	0
+undefined	NaN
+"" (empty string)	0
+"123"	123
+
+*/
+
+
+//prefix vs postfix increment operators
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
+console.log(++gameCounter);
+console.log(gameCounter++);
+
+/*
+ 1. ++a → Prefix Increment
+This increases the value of a before it is used in the expression.
+
+So ++a will increment a from 3 to 4, and returns 4.
+
+let a = 3;
+let b = ++a;  // a becomes 4, b = 4
+
+
+2. a++ → Postfix Increment
+This uses the value of a before increasing it.
+
+So a++ will return 4 but then update a to 5.
+
+let a = 4;
+let b = a++;  // b = 4, then a becomes 5
+
+*/
